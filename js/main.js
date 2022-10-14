55440e2c73f488a188529d52904986c01db38c79
Brainana <63419397+Brainana@users.noreply.github.com>
2022-10-14 19:52:59 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ function switchToMapView(enable) {
     let rightPanel =  document.querySelector("#right-panel");
     rightPanel.style.display = enable ? "block" : "none";
 }
+
+function adjustMapBound() {
+    var bounds = new google.maps.LatLngBounds();
+
+    if (startingPointMarker) {
+        bounds.extend(startingPointMarker.getPosition());
+    }
+
+    for (var memorialId in memorialSelected) {
+        bounds.extend(memorialSelected[memorialId].marker.getPosition());
+    }
+
+    map.fitBounds(bounds);
+}
